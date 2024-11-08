b4b592d8ce41d99fdd396c1afcac0dd861d69a3d
Deatheh <kazancevdanila2015@yandex.ru>
2024-11-08 18:09:58 +0300

    v0-alfa

---
+++ 1/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+var name = "Sergei"
+var name2 = "Danial"
+
+func main() {
+	fmt.Println(name, " and ", name2)
+}
---
+++ 4/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+var name = "Danial"
+
+func main() {
+	fmt.Println(name)
+}
